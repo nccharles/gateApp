7c46d04330eae0e30489dc22304d0e4fa0a91d91
charles <nccharles1@gmail.com>
2019-10-04 12:02:37 +0200

    configulate colos will be used on app

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,32 @@
-const tintColor = '#2f95dc';
-
+const tintColor = 'rgba(41, 128, 185,1.0)';
+const primary="rgba(41, 128, 185,1.0)";
+const primary_trans="rgba(41, 128, 185,0.7)";
+const secondary='rgba(39, 174, 96,1.0)';
+const third="rgba(184, 233, 148,1.0)";
+const secondary_trans='rgba(39, 174, 96,.7)';
+const primary_white="#FFFFFF";
+const primary_gray="#a4b0be";
+const primary_black="#000000"
 export default {
+  primary,
+  secondary,
+  trans:primary_trans,
+  third,
+  primary_white,
+  primary_gray,
+  primary_black,
+  primary_gradient: [third,secondary,primary],
+  trans_gradient: [primary_trans,secondary_trans],
+  Swiper_gradient:[primary,third],
+  form_gradient:[primary_white,primary_gray],
   tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
+  tabIconDefault: primary_gray,
+  tabIconSelected: primary,
+  tabBar: primary_white,
+  errorBackground: '#c23616',
+  errorText: primary_white,
   warningBackground: '#EAEB5E',
   warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+  noticeBackground: primary,
+  noticeText: primary_white,
 };
